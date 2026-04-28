fd46b4148b9888c03ee2b85474c35f06317c0772
Rob Schoen <37676779+millxing@users.noreply.github.com>
2026-04-28 17:52:24 -0400

    X and Tavern

--- src/app/components/Endorsements.tsx
+++ src/app/components/Endorsements.tsx
@@ -19,7 +19,7 @@ const featuredEndorsers = [
   { name: "Kate Poverman", title: "Former Town Moderator" },
   { name: "Sandy Gadsby", title: "Former Town Moderator" },
   { name: "Evelyn Murphy", title: "Former MA Lieutenant Governor"},
-  { name: "John Caron", title: "Assistant Moderator"},
+  { name: "John Karon", title: "Assistant Moderator"},
 ];
 
 const communityEndorsers = [
